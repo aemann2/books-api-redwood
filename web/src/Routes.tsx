64477d81f814ffc3c34f682c98c9d4cad60cda7d
aemann2 <aemann2@gmail.com>
2022-02-10 14:16:18 -0800

    protected Books page and added generated auth pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,17 +1,23 @@
 import NavLayout from './layouts/NavLayout/NavLayout'
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 import BooksLayout from 'src/layouts/BooksLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={NavLayout}>
         <Route path="/" page={HomePage} name="home" />
-        <Route path="/books/{id:Int}" page={BookBookPage} name="book" />
-        <Set wrap={BooksLayout}>
-          <Route path="/books/new" page={BookNewBookPage} name="newBook" />
-          <Route path="/books" page={BookBooksPage} name="books" />
-        </Set>
+        <Private unauthenticated="home">
+          <Route path="/books/{id:Int}" page={BookBookPage} name="book" />
+          <Set wrap={BooksLayout}>
+            <Route path="/books/new" page={BookNewBookPage} name="newBook" />
+            <Route path="/books" page={BookBooksPage} name="books" />
+          </Set>
+        </Private>
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
