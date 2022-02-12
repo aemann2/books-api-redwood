119519f0645ee65c61e6df91377751438c6a29a9
Adam Mann <68879246+aemann2@users.noreply.github.com>
2022-02-11 16:10:57 -0800

    setup/auth (#4)
    * began setting up dbAuth
    
    * added User type for auth
    
    * protected Books page and added generated auth pages
    
    * added correct layout to login page and nav layout
    
    * added logout, logged in message to nav

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import NavLayout from './layouts/NavLayout/NavLayout'
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 import BooksLayout from 'src/layouts/BooksLayout'
 
 const Routes = () => {
@@ -7,11 +7,17 @@ const Routes = () => {
     <Router>
       <Set wrap={NavLayout}>
         <Route path="/" page={HomePage} name="home" />
-        <Route path="/books/{id:Int}" page={BookBookPage} name="book" />
-        <Set wrap={BooksLayout}>
-          <Route path="/books/new" page={BookNewBookPage} name="newBook" />
-          <Route path="/books" page={BookBooksPage} name="books" />
-        </Set>
+        <Route path="/login" page={LoginPage} name="login" />
+        <Route path="/signup" page={SignupPage} name="signup" />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+        <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+        <Private unauthenticated="login">
+          <Route path="/books/{id:Int}" page={BookBookPage} name="book" />
+          <Set wrap={BooksLayout}>
+            <Route path="/books/new" page={BookNewBookPage} name="newBook" />
+            <Route path="/books" page={BookBooksPage} name="books" />
+          </Set>
+        </Private>
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
